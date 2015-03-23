f0dd13fc794b15cede6c243e0b292d8b6a2427b6
brunoendo <endo.bruno@gmail.com>
2015-03-23 10:09:57 -0300

    Add ajax request to nominatim
    Also fixed bug with jQuery val() function

--- assets/js/event_handlers.js
+++ assets/js/event_handlers.js
@@ -6,21 +6,21 @@ $(document).ready(function() {
     console.log("Button clicked");
   });
 
-  $(".address").focusout(function() {
-    getGeocodeFromAddress(this);
+  $(".address").focusout(function(element) {
+    getGeocodeFromAddress(element.target);
   });
 
   (function () {
     // TODO test if there is no problem with both text boxes using
     // lastKeypressId at the same time (possible race condition)
     var lastKeypressId = 0;
-    $(".address").keyup(function() {
+    $(".address").keyup(function(element) {
       var targetId = lastKeypressId + 1;
       lastKeypressId = targetId;
 
       setTimeout(function() {
         if (lastKeypressId == targetId) {
-          getGeocodeFromAddress(this);
+          getGeocodeFromAddress(element.target);
         } else {
           console.log("usuario digitou");
         }

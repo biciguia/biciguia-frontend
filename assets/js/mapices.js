6c8c51b4ae37dae17b05b981110baa661e9e0582
leeohaddad <leo.haddad1@gmail.com>
2015-04-07 11:58:47 -0300

    pilot: @leeohaddad, copilot: @vonpupp. Added GPL license

--- assets/js/mapices.js
+++ assets/js/mapices.js
@@ -1,3 +1,20 @@
+/*
+Copyright © 2015 Biciguia Team
+
+Biciguia is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Biciguia is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
 // create a map in the "map" div, set the view to a given place and zoom
 var map = L.map('map').setView([-23.5475, -46.63611], 13);
 

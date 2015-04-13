33b10ed1ad20f66ded207f85379cc4dfa4bf6ac2
Gustavo Caparica <gustavo.caparica@gmail.com>
2015-04-13 14:21:12 -0300

    Fix tests that broke because of the geocoder change
    signed off by: Bruno <endo.bruno@gmail.com>

--- libs/tests_opencage.js
+++ libs/tests_opencage.js
@@ -15,26 +15,86 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 
-var addressIME = [{
-  "place_id":"90628654",
-    "licence":"Data © OpenStreetMap contributors, ODbL 1.0. http://www.openstreetmap.org/copyright",
-    "osm_type":"way",
-    "osm_id":"154079142",
-    "boundingbox":["-23.5598092","-23.5589311","-46.7324506","-46.7317129"],
-    "lat":"-23.55935105",
-    "lon":"-46.732049560281",
-    "display_name":"IME - Bloco B, 1010, Rua do Matão, Butantã, São Paulo, RMSP, São Paulo, Southeast Region, 05508-090, Brazil",
-    "class":"building",
-    "type":"yes",
-    "importance":0.501
-}];
+var addressIME = {
+  "results": [
+  {
+    "annotations": {
+      "DMS": {
+        "lat": "23\u00b0 33' 33.66378'' S",
+        "lng": "46\u00b0 43' 55.37842'' W"
+      },
+      "MGRS": "23KLP2321893489",
+      "Maidenhead": "GG66pk25ds",
+      "Mercator": {
+        "x": -5202187.961,
+        "y": -2682732.013
+      },
+      "OSM": {
+        "url": "http:\/\/www.openstreetmap.org\/?mlat=-23.55935&mlon=-46.73205#map=17\/-23.55935\/-46.73205"
+      },
+      "callingcode": 55,
+      "geohash": "6gycbx4ub6dhzdvuyf65",
+      "sun": {
+        "rise": {
+          "astronomical": 1428912180,
+          "civil": 1428915360,
+          "nautical": 1428913800
+        },
+        "set": {
+          "astronomical": 1428963060,
+          "civil": 1428959880,
+          "nautical": 1428961500
+        }
+      },
+      "timezone": {
+        "name": "America\/Sao_Paulo",
+        "now_in_dst": 0,
+        "offset_sec": -10800,
+        "offset_string": -300,
+        "short_name": "BRT"
+      },
+      "what3words": {
+        "words": "supposed.nagging.topic"
+      }
+    },
+    "bounds": {
+      "northeast": {
+        "lat": -23.5589311,
+        "lng": -46.7317129
+      },
+      "southwest": {
+        "lat": -23.5598092,
+        "lng": -46.7324506
+      }
+    },
+    "components": {
+      "city": "S\u00e3o Paulo",
+      "city_district": "Butant\u00e3",
+      "country": "Brasil",
+      "country_code": "br",
+      "county": "RMSP",
+      "house": "IME - Bloco B",
+      "house_number": "1010",
+      "road": "Rua do Mat\u00e3o",
+      "state": "S\u00e3o Paulo",
+      "suburb": "Butant\u00e3"
+    },
+    "confidence": 10,
+    "formatted": "IME - Bloco B, 1010 Rua do Mat\u00e3o, S\u00e3o Paulo - SP, Brasil",
+    "geometry": {
+      "lat": -23.55935105,
+      "lng": -46.732049560281
+    }
+  }
+]};
 
 /* globals for mocking */
-var $;
+var $; /* jquery */
+var L; /* leaflet */
 
 QUnit.test("getGeocoderURLFromAddress", function (assert) {
   var result = getGeocoderURLFromAddress("Rua do Matao, 1010");
-  var expected = '//nominatim.openstreetmap.org/search?format=json&city=S%C3%A3o%20Paulo&state=S%C3%A3o%20Paulo&country=Brasil&street=Rua%20do%20Matao%2C%201010';
+  var expected = 'https://api.opencagedata.com/geocode/v1/json?key=651ad55fe59eed0f1beb0c550ab6b0d3&countrycode=BR&language=pt-BR&min_confidence=3&fields=geometry%2Ccomponents&q=Rua%20do%20Matao%2C%201010%2C%20S%C3%A3o%20Paulo';
 
   assert.ok(result == expected, "URL returned is ok");
 
@@ -56,9 +116,10 @@ QUnit.test("bind2ndArgument", function (assert) {
 });
 
 QUnit.test("getAddressListHTML", function (assert) {
-  var expected = "<li class='pure-menu-item'><a href='#' id='origin-0' class='pure-menu-link origin-suggestion-item'>IME - Bloco B, 1010, Rua do Matão, Butantã, São Paulo, RMSP, São Paulo, Southeast Region, 05508-090, Brazil</a></li>";
+  var expected = "<li class='pure-menu-item'><a href='#' id='origin-0' class='pure-menu-link origin-suggestion-item'>IME - Bloco B, 1010 Rua do Mat\u00e3o, S\u00e3o Paulo - SP, Brasil</a></li>";
 
   var result = getAddressListHTML(addressIME, 'origin');
+  console.log(result);
 
   assert.equal(result.length, 1, "Result has expected length");
 
@@ -91,7 +152,7 @@ QUnit.test("showAddressList", function (assert) {
   };
 
   $ = sinon.stub().returns(test_stubs);
-  
+
   showAddressList(addressIME, 'origin');
 
   assert.ok($.calledWith("#origin-table"), "#origin-table reached correctly");
@@ -112,7 +173,7 @@ QUnit.test("menuItemSelected", function (assert) {
   };
 
   $ = sinon.stub().returns(test_stubs);
-  
+
   var test_event = {"target": {"id": "origin-0"}};
 
   menuItemSelected(test_event, addressIME);

1adb57633024bd174f5b45fd0ae17eb82a9f0e51
Tiago Fassoni <tiagofassoni@gmail.com>
2015-05-27 16:52:30 -0300

    Solved problem with maki and Jekyll
    We are now only using the static files from maki, not its repo nor its
    JS.
    
    So we don't have issues with jekyll's preprocessor misunderstanding its
    tags.

--- libs/lib_map.js
+++ libs/lib_map.js
@@ -38,13 +38,13 @@ var overlayFiles = {
 };
 
 var overlayIcons = {
-  "Ambulatórios": "../assets/icons/src/lodging",
-  "Bibliotecas": "../assets/icons/src/town-hall",
-  "Bosques e Pontos de Leitura": "../assets/icons/src/library",
-  "Hospitais": "../assets/icons/src/city",
-  "Museus": "../assets/icons/src/museum",
-  "Pronto-Socorros": "../assets/icons/src/hospital",
-  "Unidades Básicas de Saúde": "../assets/icons/src/heart"
+  "Ambulatórios": "/assets/icons/maki/src/lodging",
+  "Bibliotecas": "/assets/icons/maki/src/town-hall",
+  "Bosques e Pontos de Leitura": "/assets/icons/maki/src/library",
+  "Hospitais": "/assets/icons/maki/src/city",
+  "Museus": "/assets/icons/maki/src/museum",
+  "Pronto-Socorros": "/assets/icons/maki/src/hospital",
+  "Unidades Básicas de Saúde": "/assets/icons/maki/src/heart"
 };
 
 function initializeMap(){
